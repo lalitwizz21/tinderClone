7ce7a6f2f34c79d5fe4de41363c6b43ad3541456
Lalit Rathore <lalit.rathore@tv2consulting.com>
2025-10-02 12:40:06 +0530

    Added below routes: - To get all the users. - To get user by Id. - To get user by email. - To delete a user. - To update a user bv Id - To update a user bv email

--- src/app.js
+++ src/app.js
@@ -24,6 +24,93 @@ app.post('/signup', async (req, res) => {
   }
 });
 
+// Get the user list.
+app.get("/feed", async (req, res) => {
+  try {
+    const userList = await User.find();
+    if (userList?.length > 0) {
+      res.json(userList)
+    } else {
+      res.status(404).send("No user Found.")
+    }
+  } catch (e) {
+    res.status(400).send("Something went wrong " + e?.message)
+  }
+});
+
+// Find user by id.
+app.get("/user/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const user = await User.findById(id);
+    if (user) {
+      res.status(200).json(user)
+    } else {
+      res.status(404).send("User not found")
+    }
+  } catch (e) {
+    res.status(400).send("Something went wrong " + e?.message)
+  }
+})
+
+// Fetch user by email
+app.get("/user/:mail", async (req, res) => {
+  const mail = req.params.mail;
+
+  try {
+    const user = await User.find({ email: mail });
+    if (user?.length) {
+      res.status(200).json(user)
+    } else {
+      res.status(404).send("User not found")
+    }
+  } catch (e) {
+    res.status(400).send("Something went wrong " + e?.message)
+  }
+})
+
+//  delete a user by id.
+app.delete('/user/:id', async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const deletedUSer = await User.findByIdAndDelete(id);
+    res.status(200).json(deletedUSer)
+  } catch (e) {
+    res.status(400).send("Something went wrong " + e?.message)
+  }
+});
+
+// Update a user by Id.
+app.patch("/user", async (req, res) => {
+  const id = req.body.id;
+  const userData = req.body;
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(id, userData, {
+      returnDocument: "after"
+    });
+    res.json(updatedUser);
+  }
+  catch (e) {
+    res.status(400).send("Something went wrong " + e?.message)
+  }
+});
+
+// Update a user by email.
+app.patch("/userByMail", async (req, res) => {
+  const mail = req.body.mail;
+  const userData = req.body;
+
+  try {
+    const updatedUser = await User.findOneAndUpdate({ email: mail }, userData);
+    res.json(updatedUser)
+  } catch (e) {
+    res.status(400).send("Something went wrong " + e?.message)
+  }
+})
+
 // Connecting to the DB.
 connectToDB().then(() => {
   console.log("DB is successfully connected.")
